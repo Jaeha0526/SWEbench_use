6b84ee630f288dfb35f7d503f81b133157b7e0fb
Jaeha Lee <132727115+Jaeha0526@users.noreply.github.com>
2024-12-14 21:30:37 -0800

    venv

--- swebench-env/bin/activate.fish
+++ swebench-env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jaeha/SWE-bench/swebench-env"
+set -gx VIRTUAL_ENV "/Users/jaeha/Toolformer_project/SWE-bench/swebench-env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
